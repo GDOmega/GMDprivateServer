155cd5d58fafab189adcbe2ddcf41aa61c8c5d12
fancymelonhub <30706150+fancythedeveloper@users.noreply.github.com>
2021-11-21 23:37:13 -0500

    lets try this

--- incl/levels/suggestGJStars.php
+++ incl/levels/suggestGJStars.php
@@ -5,7 +5,12 @@ include "../lib/connection.php";
 require_once "../lib/GJPCheck.php";
 require_once "../lib/exploitPatch.php";
 require_once "../lib/mainLib.php";
+require_once "../lib/webhooks/webhook.php";
 $gs = new mainLib();
+$uname = $gs->getAccountName($accountID);
+$res = $queryNAME->fetchAll();
+$aLevelName = $res[0]["levelName"];
+$aUserName = $res[0]["userName"];
 
 $gjp = ExploitPatch::remove($_POST["gjp"]);
 $stars = ExploitPatch::remove($_POST["stars"]);
@@ -18,6 +23,12 @@ if($gs->checkPermission($accountID, "actionRateStars")){
 	$gs->rateLevel($accountID, $levelID, $stars, $difficulty["diff"], $difficulty["auto"], $difficulty["demon"]);
 	$gs->featureLevel($accountID, $levelID, $feature);
 	$gs->verifyCoinsLevel($accountID, $levelID, 1);
+        if ($feature) {
+	$featurestr = "Yes";
+	} else {
+	$featurestr = "No";
+	}
+        PostToHook("Command - Rate", "$uname rated $aLevelName by $aUserName ($levelID).\nStars: $stars\nDifficulty: $difficulty\nFeatured: $featurestr\nCoins: Yes");
 	echo 1;
 }else if($gs->checkPermission($accountID, "actionSuggestRating")){
 	$gs->suggestLevel($accountID, $levelID, $difficulty["diff"], $stars, $feature, $difficulty["auto"], $difficulty["demon"]);
